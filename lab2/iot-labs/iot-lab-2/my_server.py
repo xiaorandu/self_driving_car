bd0471edf819d67b5efa4e310856eba888f12d67
bcentner <bcentner@umich.edu>
2023-10-04 20:09:14 -0500

    Fix structure4

--- lab2/iot-labs/iot-lab-2/my_server.py
+++ lab2/iot-labs/iot-lab-2/my_server.py
@@ -6,8 +6,20 @@ HOST = "192.168.12.232" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
 
-def drive_car(direction: str):
+def drive_car(car: Picar, direction: str):
     print(f"Drive car str:\t{direction}")
+    if direction == '87':
+        print("Forward")
+        car.forward()
+    elif direction == '83':
+        print("Backward")
+        car.backward()
+    elif direction == '65':
+        print("Left")
+        car.move_left()
+    elif direction == '68':
+        print("Right")
+        car.move_right()
 
 
 
@@ -23,9 +35,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print("server recv from: ", clientInfo)
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
             if data != b"":
-                print(str(data))
+                print(data)
                 
-                drive_car(data)
+                drive_car(car, data.decode('utf-8'))
 
                 client.sendall(data) # Echo back to client
     except: 
